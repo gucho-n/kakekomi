4bc5e38bd622de61f7f19ac4f1ba705d27f026a8
gucho-n <i.m.your.dog.1192@gmail.com>
2021-09-23 15:59:15 +0900

    リセットCSSを繋ぐ destyle.cssの導入

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップ画面</title>
+
+    <!-- リセットCSSを繋ぐ destyle.cssの導入-->
+    <!-- https://github.com/nicolas-cusan/destyle.css/blob/master/destyle.css -->
+    <link rel="stylesheet" href="{{ asset('css/base/destyle.css') }}">
     <!-- baseviewの大元の記述 -->
     <link rel="stylesheet" href="{{ asset('css/base/base.css') }}">
 </head>
